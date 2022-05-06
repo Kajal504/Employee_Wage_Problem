76b06eafcb2c62beb2260c442e0285a26f0fb7d2
PRAMODSHENDAGE <shendagepramod@gmail.com>
2022-05-06 15:31:46 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+UC1_EmplyeeAttendance.EmployeeAttendance();
+UC2_EmployeeWage.EmployeeWage();
+UC3_PartTimeEmployeeWage.EmployeeWage();
+UC4_EmpWageUsingSwitch.EmployeeWage();
+UC5_EmpWageFor20WorkingDays.EmpWageFor20Days();
+UC6_EmpWageForMax100Hrs.EmpWageForMax100Hrs();
